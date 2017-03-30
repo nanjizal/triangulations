769f2f9f44ce9fea8cc6ab43ba1fc8fee937acc4
nanjizal <nitsuj@nanjizal.net>
2017-03-30 17:32:04 +0100

    Update Triangulate.hx

--- triangulations/Triangulate.hx
+++ triangulations/Triangulate.hx
@@ -85,7 +85,52 @@ class triangulate {
     }
 
     //TODO: continue port
-    public inline static function faces( vertices: Array<Vertex>, faces: Array<Vector2D> ){
-    
-    }
+    public inline static function faces( vertices: Array<Vertex>, faces: Array<Vector2> ){
+        // Convert the polygon components into linked lists. We assume the first
+        // polygon is the outermost, and the rest, if present, are holes.
+        var polies = [ makeLinkedPoly( faces[ 0 ] ) ];
+        var holes = [];
+        for (var k = 1; k < face.length; ++k) {
+          holes.push( makeLinkedPoly( face[ k ] ) );
+        }
+        
+        // We handle only the outer polygons. We start with only one, but more are
+        // to come because of splitting. The holes are eventually merged in.
+        // In each iteration a diagonal is added.
+        var diagonals = [];
+        while (polies.length > 0) {
+            var poly = polies.pop();
+
+            // First we find a locally convex vertex.
+            var node = poly;
+            var a: Vector2D;
+            var b: Vector2D;
+            var c: Vector2D;
+            var convex = false;
+            do {
+                a = vertices[node.prev.value];
+                b = vertices[node.value];
+                c = vertices[node.next.value];
+                convex = (a.span(b)).cross(b.span(c)) < 0;
+                node = node.next;
+            } while(!convex && node !== poly);
+
+            if(!convex) continue;
+            var aNode = node.prev.prev;
+            var bNode = node.prev;
+            var cNode = node;
+
+            // We try to make a diagonal out of ac. This is possible only if it lies
+            // completely inside the polygon.
+            var acOK = true;
+
+            // Ensuring there are no intersections of ac with other edges doesn't
+            // guarantee that ac lies within the poly. It is also possible that the
+            // whole polygon is inside the triangle abc. Therefore we early reject the
+            // case when the immediate neighbors of vertices a and c are inside abc.
+            // Note that if ac is already an edge, it will also be rejected.
+            var inabc = Geom2.pointInTriangle(a, b, c);
+            acOK = !inabc(vertices[aNode.prev.i]) && !inabc(vertices[cNode.next.i]);
+            //TODO: continue port        
+        }
 }
