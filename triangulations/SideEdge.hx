23c1f5c185afcd38e2534550fa702834a5a4415c
nanjizal <nitsuj@nanjizal.net>
2017-04-03 11:53:17 +0100

    Create SideEdge.hx

---
+++ triangulations/SideEdge.hx
@@ -0,0 +1,55 @@
+package triangulations
+
+class SideEdge {
+    public var length: Int = 4;
+    public var a: Int;
+    public var b: Int;
+    public var c: Int;
+    public var d: Int;
+    public var curr: a;
+    public function new( a_: Int, b_: Int, c_: Int, d_: Int ){
+        a = a_;
+        b = b_;
+        c = c_;
+        d = d_;
+    }
+    public static inline
+    function iterator ():Iterator<Int> { 
+        count = 0;
+        return this;
+    }
+    public static inline
+    public function hasNext(): Bool {
+        return count < length;
+    }
+    public static inline
+    function next(): Int { 
+        var out: Int;
+        switch( count ){
+            case 0:
+                out = a;
+            case 1:
+                out = b;
+            case 2:
+                out = c;
+            case 3:
+                out = d;
+            default:
+                out = a;
+        }
+        return return out; 
+    }
+    public static inline
+    function substitute( x: Int, y: Int ) {
+        switch( x ){
+            case a:
+                a = y;
+            case b:
+                b = y;
+            case c:
+                c = y;
+            default:
+                d = y;
+        }
+    }
+}
