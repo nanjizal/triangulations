dcd19d865e1a1d2091f3c0a4e700f2c061da76ac
nanjizal <nitsuj@nanjizal.net>
2017-04-05 14:44:40 +0100

    Update Edges.hx

--- triangulations/Edges.hx
+++ triangulations/Edges.hx
@@ -2,9 +2,11 @@ package triangulations;
 import triangulations.Geom2;
 
 abstract Edges( Array<Edge> ) from Array<Edge> to Array<Edge> {
+    
     inline public function new( v: Array<Edge> ) {
       this = v;
     }
+    
     public /*inline*/ function set_fixedExternal( val: Bool ){
         for( e in this ) {
             e.fixed = val;
@@ -13,6 +15,7 @@ abstract Edges( Array<Edge> ) from Array<Edge> to Array<Edge> {
         return val;
     }
     public var fixedExternal( default, set ):Bool;
+    
     public inline
     function clone(): Edges {
         var e = new Edges();
@@ -44,4 +47,5 @@ abstract Edges( Array<Edge> ) from Array<Edge> to Array<Edge> {
         var el = e.length;
         for( i in 0...el ) this[ l + i ] = e[ i ];
     }
+    
 }
