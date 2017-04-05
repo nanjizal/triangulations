e4a59c4ebc317f16a0a48fd8ad79a29014954aaf
nanjizal <nitsuj@nanjizal.net>
2017-04-05 14:29:25 +0100

    Update Edges.hx

--- triangulations/Edges.hx
+++ triangulations/Edges.hx
@@ -23,4 +23,25 @@ abstract Edges( Array<Edge> ) from Array<Edge> to Array<Edge> {
         }
         return v;
     }
+    
+    public inline
+    function getUnsure(): Array<Int> {
+        var unsureEdges = Array<Int>();
+        var l = this.length;
+        var lu = 0;
+        for( j in 0...l ){
+            if( !edges[j].fixed )
+                unsureEdges[ lu ] = j;
+                lu++;
+            }
+        }
+        return unsureEdges;
+    }
+    
+    public inline
+    function add( e: Edges ){
+        var l = this.length;
+        var el = e.length;
+        for( i in 0...el ) this[ l + i ] = e[ i ];
+    }
 }
