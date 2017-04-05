99d6b86576a92ed6d5ca0b8a97d8fd47d45112a7
nanjizal <nitsuj@nanjizal.net>
2017-04-05 17:09:23 +0100

    Create MainTestSetup.hx

---
+++ tests/MainTestSetup.hx
@@ -0,0 +1,20 @@
+package tests;
+import triangulations.Edge;
+import triangulations.Edges;
+import triangulations.Vertices;
+import triangulations.Geom2;
+import triangulations.Node;
+import triangulations.Queue;
+import triangulations.SideEdge;
+import triangulations.Settings;
+class MainTestSetup {
+    
+    static function main(){
+        new MainTestSetup();
+    }
+    
+    public function new(){
+        trace( 'Testing Triangulations ');
+    }
+    
+}
