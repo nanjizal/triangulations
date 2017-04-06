097ae1fa3649634b7764f68c7e1b3af3314d72cb
nanjizal <nitsuj@nanjizal.net>
2017-04-06 16:54:26 +0100

    Create FillShape.hx

---
+++ triangulations/FillShape.hx
@@ -0,0 +1,11 @@
+package triangulations;
+import triangulations.Vertices;
+import triangulations.Edges;
+import triangulations.Face;
+
+class FillShape {
+    public var vertices: Vertices;
+    public var edges: Edges;
+    public var faces: Array<Array<Face>>;
+    public function new(){}
+}
