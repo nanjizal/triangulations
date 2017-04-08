f40e876407c60bfc8e9d9062626f4a147ea07fe4
nanjizal <nitsuj@nanjizal.net>
2017-04-08 12:53:33 +0100

    Create Angle_compare.hx

---
+++ tests/fillShapes/Angle_compare.hx
@@ -0,0 +1,22 @@
+package tests.fillShapes;
+import triangulations.FillShape;
+import triangulations.Face;
+
+abstract Angle_compare( FillShape ) from FillShape to FillShape {
+    inline public function new(){
+        this = new FillShape();
+        this.vertices = [
+                        [200., 200.],
+                        [100., 300.],
+                        [300., 200.],
+                        [200., 100.]
+                        ];
+        this.edges = [
+                    [0, 1],
+                    [0, 2],
+                    [0, 3]
+                    ];
+        var face: Face = [];
+        this.faces = [[face]];
+    }
+}
