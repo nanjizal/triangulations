26b5a916da5c7e7ca813e9cf11d0f22ec1770a64
nanjizal <nitsuj@nanjizal.net>
2017-04-14 11:13:47 +0100

    Update FillShape.hx

--- triangulations/FillShape.hx
+++ triangulations/FillShape.hx
@@ -8,4 +8,12 @@ class FillShape {
     public var edges: Edges;
     public var faces: Array<Array<Face>>;
     public function new(){}
+    public inline 
+    function scale( s: Float ){
+        vertices.scale( s );
+    }
+    public inline 
+    function translate( x: Float, y: Float ){
+        vertices.translate( x, y );
+    }
 }
