27c67f933b57389dfd9ce9883bc9ae1b2d954485
nanjizal <nitsuj@nanjizal.net>
2017-04-18 11:05:29 +0100

    change add to return the edge

--- triangulations/Edges.hx
+++ triangulations/Edges.hx
@@ -45,10 +45,11 @@ abstract Edges( Array<Edge> ) from Array<Edge> to Array<Edge> {
     }
     
     public inline
-    function add( e: Edges ){
+    function add( e: Edges ): Edges {
         var l = this.length;
         var el = e.length;
         for( i in 0...el ) this[ l + i ] = e[ i ];
+        return e;
     }
     @:from
     static public function fromArrayArray( arr: Array<Array<Null<Int>>> ) {
