82c0928b7509c72b6cd11236762a908a02bc658b
nanjizal <nitsuj@nanjizal.net>
2017-04-19 10:14:32 +0100

    Update Face.hx

--- triangulations/Face.hx
+++ triangulations/Face.hx
@@ -1,7 +1,12 @@
 package triangulations;
 @:forward
 abstract Face( Array<Int> ) from Array<Int> to Array<Int> {
-    inline public function new( v: Array<Int> ) {
+    inline public function new( ?v: Array<Int> ) {
+      if( v == null ) v = getEmpty();
       this = v;
+    }
+    public inline static 
+    function getEmpty(){
+        return new Face( new Array<Int>() );
     }
 }
