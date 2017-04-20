3a35416f39f86f6d45ddf35bc9ac29d3a6900c4e
nanjizal <nitsuj@nanjizal.net>
2017-04-20 15:58:58 +0100

    changed so outputs the actual face.
    bit crazy otherwise?

--- triangulations/FindEnclosingTriangle.hx
+++ triangulations/FindEnclosingTriangle.hx
@@ -3,6 +3,7 @@ import triangulations.Vertices;
 import triangulations.Edges;
 import triangulations.SideEdge;
 import triangulations.Queue;
+import triangulations.Face;
 import khaMath.Vector2;
   
 // This is not really nice to understand :(
@@ -64,7 +65,7 @@ class FindEnclosingTriangles {
                                   ,     sideEdges_:  Array<SideEdge>
                                   ,     p:          Vector2
                                   ,     j0:         Int 
-                                  ):    Void->Null<Int> {
+                                  ):    Void->Face {
         return function() {
             edges = edges_;
             coEdges = coEdges_;
@@ -74,6 +75,7 @@ class FindEnclosingTriangles {
             // We start at two triangles adjecent to edge j.
             tryEnqueue( j0, 0 ); 
             tryEnqueue( j0, 1 ); 
+            var t_ = null;
             while( !queue.isEmpty() ){
                 var t = queue.dequeue();
                 var k = t % 2;
@@ -89,9 +91,12 @@ class FindEnclosingTriangles {
                 var b = vertices[bi];
                 var ci = edges[j].q;  
                 var c = vertices[ci];
-                if( Geom2.pointInTriangle(a, b, c)( p ) ) return t;
-                // Continue search to triangles adjecent to edges opposite to vertices a and
-                // c. The other triangle, adjecent to edge j, i.e., oppisite to b, is not
+                if( Geom2.pointInTriangle(a, b, c)( p ) ) {
+                    t_ = t;
+                    break;
+                } 
+                // Continue search to triangles adjacent to edges opposite to vertices a and
+                // c. The other triangle, adjacent to edge j, i.e., opposite to b, is not
                 // further examined as this is the direction we are coming from.
                 var ja = sideEdges[j].getByIndex( k ); // :(
                 var jc = sideEdges[j].getByIndex( 3 - k );
@@ -99,7 +104,19 @@ class FindEnclosingTriangles {
                 if( !edges[ja].fixed ) tryEnqueue( ja, coEdges[ja].p == ai ? 1 : 0 );
                 if( !edges[jc].fixed ) tryEnqueue( jc, coEdges[jc].p == ci ? 1 : 0 );
             }
-            return null;
+            var face: Face = null;
+            if ( t_ != null ) {
+                var k = Std.int( t_ % 2 );
+                var j = Std.int( (t_ - k) / 2 );
+                var c: Int;
+                if( k == 0 ){
+                    c = coEdges[j].p;
+                } else {
+                    c = coEdges[j].q;
+                }
+                face = [ edges[j].p, edges[j].q, c ];
+            }
+            return face;
         };
     }
 }
