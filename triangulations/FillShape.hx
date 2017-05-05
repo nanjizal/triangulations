77844eb961ee53bbe00fe2d52ce8cc4e30ad5b86
nanjizal <nitsuj@nanjizal.net>
2017-05-05 13:16:28 +0100

    add clone

--- triangulations/FillShape.hx
+++ triangulations/FillShape.hx
@@ -2,7 +2,7 @@ package triangulations;
 import triangulations.Vertices;
 import triangulations.Edges;
 import triangulations.Face;
-
+import khaMath.Vector2;
 class FillShape {
     public var vertices: Vertices;
     public var edges: Edges;
@@ -23,5 +23,30 @@ class FillShape {
     public inline
     function set_fixedExternal( val: Bool ){
         edges.set_fixedExternal( val );
+    }
+    public inline
+    function clone(){
+        var f = new FillShape();
+        f.edges     = edges.clone();
+        f.vertices  = vertices.clone();
+        f.faces     = cloneFaces();
+        return f;
+    }
+    public inline
+    function cloneFaces(){
+        var f = new Array<Array<Face>>();
+        var l0 = faces.length;
+        var l1: Int;
+        var arrFace: Array<Face>;
+        for( i in 0...l0 ){
+            var temp = new Array<Face>();
+            arrFace = faces[i];
+            l1 = arrFace.length;
+            for( j in 0...l1 ){
+                temp[j] = arrFace[j].clone();
+            }
+            f[i] = temp;
+        }
+        return f;
     }
 }
