6208addc1dbabb42d126ace1d2625da5bc8aa249
nanjizal <nitsuj@nanjizal.net>
2017-05-11 16:04:03 +0100

    Update EdgeVertexTriangle.hx

--- triangulations/EdgeVertexTriangle.hx
+++ triangulations/EdgeVertexTriangle.hx
@@ -1,16 +1,15 @@
 package triangulations;
-
-    // Created abstraction to make 't' cleaner to use.
-    // Quote from 'findingEnclosedTriangle'
-    // Indexing triangles here involves some evil hacking. 
-    // A triangle is represented by an edge and a vertex of its co-edge.
-    // Suppose the edge in question has number j, and k is 0 or 1 depending on which
-    // co-edge vertex is chosen. Then the triangle index is t = 2 * j + k.
-    
-    // typedef and abstract used to represent j, edgeId and k, vertexId
-    
+/**
+ * Created abstraction to make 't' cleaner to use.
+ * Quote from 'findingEnclosedTriangle'
+ * Indexing triangles here involves some evil hacking. 
+ * A triangle is represented by an edge and a vertex of its co-edge.
+ * Suppose the edge in question has number j, and k is 0 or 1 depending on which
+ * co-edge vertex is chosen. Then the triangle index is t = 2 * j + k.
+ *   
+ * typedef and abstract used to represent j, edgeId and k, vertexId
+ **/   
 typedef EdgeVertex = { edgeId: Null<Int>, vertexId: Null<Int> };
-
 @:forward
 abstract EdgeVertexTriangle( EdgeVertex ) from EdgeVertex to EdgeVertex {
     inline public function new( e: Int, v: Int ){
