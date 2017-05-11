657c5eb97f245956855163a4318b4d0fbe8509c0
nanjizal <nitsuj@nanjizal.net>
2017-05-11 14:57:43 +0100

    improve documentation

--- triangulations/Face.hx
+++ triangulations/Face.hx
@@ -1,4 +1,7 @@
 package triangulations;
+/**
+ * Face is an abstract of an array of Vertices indicies.
+ **/
 @:forward
 abstract Face( Array<Int> ) from Array<Int> to Array<Int> {
     public inline 
@@ -6,17 +9,21 @@ abstract Face( Array<Int> ) from Array<Int> to Array<Int> {
       if( v == null ) v = getEmpty();
       this = v;
     }
+    /** 
+     * allows easier creation of an empty Face
+     **/
     public static inline 
     function getEmpty(){
         return new Face( new Array<Int>() );
     }
+    /** 
+     * clones a face, so for instance with a FillShape so only a copy of a Face is changed
+     **/
     public inline
     function clone(){
         var f: Face = getEmpty();
         var l = this.length;
-        for( i in 0...l ){
-            f[i] = this[i];
-        }
+        for( i in 0...l ) f[i] = this[ i ];
         return f;
     }
 }
