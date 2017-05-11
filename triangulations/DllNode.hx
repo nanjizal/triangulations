8d5c711905973a5ff27069189e60ad1f3d16943d
nanjizal <nitsuj@nanjizal.net>
2017-05-11 15:47:52 +0100

    improve documentation

--- triangulations/DllNode.hx
+++ triangulations/DllNode.hx
@@ -1,13 +1,33 @@
 package triangulations;
-// Double Linked List Node
+/**
+ * typedef to double linked list of Int
+ **/
 typedef DllNodeInt = DllNode<Int>;
+/**
+ * Double Linked List Node
+ **/
 class DllNode<T> {
+    /**
+     * previous Node
+     **/
     public var prev: DllNode<T>;
+    /** 
+     * next node
+     **/
     public var next: DllNode<T>;
+    /**
+     * node value 
+     **/
     public var value: Null<T>;
+    /**
+     * constructor
+     **/
     public function new( value_: T ){
         value = value_;
     }
+    /**
+     * custom toString for easier debug
+     **/
     @:keep
     public function toString() {
         var p = prev.value;
